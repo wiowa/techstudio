137f1fb1a3bebd311b5f7937739f6563b69e5971
Payen Edgar <edgar.payen@believe.com>
2025-11-14 00:12:55 +0100

    feat(mymotus): add micro app

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,12 @@ export default [
   ...nx.configs['flat/typescript'],
   ...nx.configs['flat/javascript'],
   {
-    ignores: ['**/dist', '**/test-output'],
+    ignores: [
+      '**/dist',
+      '**/test-output',
+      '**/vite.config.*.timestamp*',
+      '**/vitest.config.*.timestamp*',
+    ],
   },
   {
     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
